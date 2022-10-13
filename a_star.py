78531e16477d328495b140effc3179bfdf0a4fc3
Camilo Andres Alvis Bautista <kmilo7204@gmail.com>
2022-10-13 13:28:10 -0500

    Readability
    Signed-off-by: Camilo Andres Alvis Bautista <kmilo7204@gmail.com>

--- a_star.py
+++ a_star.py
@@ -36,12 +36,11 @@ class AStar:
 
     def get_start(self):
         """Return the position of the start node"""
-
         return self.start
 
     def get_end(self):
         """Return the position of the end node"""
-        return self.end 
+        return self.end
 
     def set_start(self, start: list):
         """
@@ -61,16 +60,19 @@ class AStar:
         """
         self.end = end
 
-    def init_open(self):
+    def init_open(self): # Open list initialization, this could be more clear.
         """
         Initialize the open list with the initial position or
         start node
         """
-        g_cost = 0 
-        distance = self.calculate_distance(self.start)
-        f_cost = g_cost + distance  
+        # Coste real del camino recorrido desde el nodo inicial g_cost
+        g_cost = 0 # What is g_cost
+        # Valor o funcion que representa el valor heuristico de distancia.
+        distance = self.calculate_distance(self.start) # What means distance (Heuristic) ?
+        # Funcion de evaluacion f_cost
+        f_cost = g_cost + distance
 
-        # Cleaning the open list 
+        # Cleaning the open list
         self.open_ls = []
 
         # Initial node appending -> [g_cost, f_cost, X , Y]
@@ -80,10 +82,11 @@ class AStar:
         """Return the second element of the given list (For sorting)"""
         return search_ls[1]
 
+    # This could be mapped to the heuristic function directly (Renamed)
     def calculate_distance(self, cell: list):
         """
         Return the distance between the desired cell to the goal
-        
+
         Keyword arguments:
         cell -- desired cell for ditance calculation
         """
@@ -96,29 +99,29 @@ class AStar:
         Keyword arguments:
         cell -- desired cell for finding the neighbors
         """
-        x = cell[2] 
-        y = cell[3] 
+        x = cell[2]
+        y = cell[3]
 
         # Create a list of the desire movements
         neighbors_ls = [
             [x, y + 1], # Right
-            [x, y - 1],  # Left 
+            [x, y - 1], # Left
             [x - 1, y], # Up
-            [x + 1, y] # Down
+            [x + 1, y]  # Down
         ]
-        return neighbors_ls    
+        return neighbors_ls
 
     def reconstruct_path(self, path_set: set, current: tuple):
         """
-        Reconstructs the path from the end position to the start 
+        Reconstructs the path from the end position to the start
 
-        The incoming set, contains the relation of the current node 
+        The incoming set, contains the relation of the current node
         and the prior one, the key and values are mapped to get the
         corresponding trajectory
 
         Keyword arguments:
         path_set -- set containing the relation between cells
-        current -- value from where searching in the path_set will start 
+        current -- value from where searching in the path_set will start
         """
 
         path_ls = []
@@ -128,10 +131,10 @@ class AStar:
         while current in path_set:
             """
             As soon as we enter in the set we extract the key of the current node
-            (It means we extract the node which expanded the current one) and 
+            (It means we extract the node which expanded the current one) and
             re-assing this one to keep looking until we reach the start node
             """
-            # Get the preceding node of the current one 
+            # Get the preceding node of the current one
             current = path_set[current]
             path_ls.append([current[0], current[1]])
 
@@ -148,34 +151,37 @@ class AStar:
                 1. Select the node with the lowest F-Score
                 2. Remove the current node from the open list (Open set)
                 3. For each neighbor of current
-                4. Calculate a tentative G-Score 
+                4. Calculate a tentative G-Score
                 5. If tentative G-Score is better than the previous one:
                     Update the new path (As we find and optimal one)
                     Calculae the new G value and the F value
                     5.1 If the node has not been visited:
                         Add it to the open set
         """
+        # Initialization of the open list
         self.init_open()
 
+        # Initialization of the close list
         close_ls = [[0 for i in range(len(self.grid[0]))] for j in range(len(self.grid))]
 
-        f_score = [[float("inf") for i in range(len(self.grid))] for j in range(len(self.grid[0]))]
+        # Infite values for all the nodes in intialization
         g_score = [[float("inf") for i in range(len(self.grid))] for j in range(len(self.grid[0]))]
+        f_score = [[float("inf") for i in range(len(self.grid))] for j in range(len(self.grid[0]))]
 
-        # Set contianing the path
+        # Set contianing the path - Because we do not want to have duplicated nodes here
         prior_set = {}
 
         # List for the visited nodes
         visited_ls = []
-        visited_ls.append(self.start)
+        visited_ls.append(self.start) # Initial node is Visited hence we append it
 
         # Initial G and F scores considering the heuristics
-        g_score[self.start[0]][self.start[1]] = 0
+        g_score[self.start[0]][self.start[1]] = 0 # No cost of moving to the initial node
+        # Full cost (evaluation function)
         f_score[self.start[0]][self.start[1]] = g_score[self.start[0]][self.start[1]] + self.calculate_distance(self.start)
 
         # Updating the visited nodes grid
         close_ls[self.start[0]][self.start[1]] = 1
-
 
         while True:
             if (len(self.open_ls) == 0):
@@ -186,29 +192,27 @@ class AStar:
             else:
                 # Step 1: The node with the lowest F value will be expanded
                 self.open_ls.sort(key=self.get_element, reverse=True)
-                
+
                 # Step 2: Delete the select node from the open list
-                current = self.open_ls.pop() 
-                
+                current = self.open_ls.pop()
+
                 # Stop condition for when the goal was found
                 if ((current[2] == self.end[0]) and (current[3] == self.end[1])):
                     print("[bold green]===> Path was found at: {}, {} <===[/bold green]".format(current[2], current[3]))
-
-                    # print("Path was found at: {}, {}".format(current[2], current[3]))                    
                     path_ls = self.reconstruct_path(prior_set, (self.end[0], self.end[1]))
                     return [path_ls, visited_ls]
 
-                # Step 3: For each neighbor of the current cell evaluate the conditions 
-                neighbors_ls = self.get_neighbors(current)                
+                # Step 3: For each neighbor of the current cell evaluate the conditions
+                neighbors_ls = self.get_neighbors(current)
                 for action in neighbors_ls:
-                    # Extract the new cell position 
+                    # Extract the new cell position
                     new_x = action[0]
                     new_y = action[1]
 
                     # Grid dimensions assertion and free obstacle checking
                     if ((new_x >= 0) and (new_x < len(self.grid)) and (new_y >= 0) and (new_y < len(self.grid[0])) and (self.grid[new_x][new_y] == 0)):
                         # Step 4: Calculate a tentative G-Score (Cost of the motion)
-                        prov_g = current[0] + 1
+                        prov_g = current[0] + 1 # Here the cost of motion os only 1, it could be different
 
                         visited_ls.append([new_x, new_y])
                         # Step 5: If tentative G-Score is better than the previous one
@@ -283,7 +287,7 @@ def main():
                     }
 
                     print("[blue]{}[/blue]".format(help_msg))
-                   
+
                 elif event.key == K_SPACE and action == 2:
                     # We we try to draw with different obstacles
                     if (planner.get_start() and planner.get_end() and redraw):
@@ -292,7 +296,7 @@ def main():
                         # Drawing start and end location
                         grid_game.draw_start(grid_game.get_start())
                         grid_game.draw_end(grid_game.get_end())
-                    
+
                     planner.set_grid(grid_game.get_grid())
                     # Space will start the searching algorithm
                     print("[bold yellow]||===========================||[/bold yellow]")
